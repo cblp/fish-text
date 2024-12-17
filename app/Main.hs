1e05f9a5ea92b17307657470af73351bf15fe34e
Yuriy Syrovetskiy <git-orbi@cblp.su>
2024-12-17 19:09:13 +0100

    Style

--- app/Main.hs
+++ app/Main.hs
@@ -1,41 +1,33 @@
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE DisambiguateRecordFields #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
-import Control.Concurrent
-import Control.Exception
-import Control.Monad
-import System.Environment
-
-import Network.HTTP.Client.TLS (newTlsManagerWith, tlsManagerSettings)
-import Servant.Client (
-  ClientM,
-  mkClientEnv,
-  runClientM,
- )
-
 import Configuration.Dotenv (defaultConfig, loadFile)
+import Control.Exception (throwIO)
 import Control.Lens ((.~), (^.))
+import Control.Monad (forever, void)
 import Data.Aeson (FromJSON)
 import Data.Aeson qualified as Aeson
 import Data.Function ((&))
-import Data.String
-import Data.Text as T
+import Data.String (fromString)
+import Data.Text (Text)
 import GHC.Generics (Generic)
+import Network.HTTP.Client.TLS (newTlsManagerWith, tlsManagerSettings)
 import Network.Wreq (defaults, getWith, param, responseBody)
+import Servant.Client (ClientM, mkClientEnv, runClientM)
+import System.Environment (getEnv)
 import Telegram.Bot.API qualified as Api
+import Time (minute, threadDelay)
 
 makePost :: Text -> IO ()
 makePost txt = do
-  chId <- Api.SomeChatId . Api.ChatId . read <$> getEnv "TELEGRAM_BOT_CHANNEL_ID"
+  chId <-
+    Api.SomeChatId . Api.ChatId . read <$> getEnv "TELEGRAM_BOT_CHANNEL_ID"
   token <- fromString <$> getEnv "TELEGRAM_BOT_TOKEN"
-  void $
-    runTelegramClient token $
-      Api.sendMessage $
-        Api.defSendMessage chId txt
+  void $ runTelegramClient token $ Api.sendMessage $ Api.defSendMessage chId txt
 
 newtype FishTextResponse = FishTextResponse {text :: Text}
   deriving anyclass (FromJSON)
@@ -44,22 +36,17 @@ newtype FishTextResponse = FishTextResponse {text :: Text}
 getPostText :: IO Text
 getPostText = do
   resp <-
-    getWith
-      (defaults & param "number" .~ ["1"])
-      "http://fish-text.ru/get"
-  obj <-
-    Aeson.throwDecode $
-      resp ^. responseBody
-
-  pure $ text obj
+    getWith (defaults & param "number" .~ ["1"]) "https://fish-text.ru/get"
+  FishTextResponse{text} <- Aeson.throwDecode $ resp ^. responseBody
+  pure text
 
 main :: IO ()
 main = do
   loadFile defaultConfig
-  forever $ do
+  forever do
     txt <- getPostText
     makePost txt
-    threadDelay 300000000
+    threadDelay $ minute 5
 
 runTelegramClient :: Api.Token -> ClientM a -> IO a
 runTelegramClient token action = do
